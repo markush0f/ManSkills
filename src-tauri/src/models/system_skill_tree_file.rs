9a368762d083fc5ecde659e05d41f9c5fc032a37
Markus <abramianmedina@gmail.com>
2026-06-10 01:32:09 +0100

    refactor(tauri): remove duplicated skill types

--- src-tauri/src/models/system_skill_tree_file.rs
+++
@@ -1,9 +0,0 @@
-use serde::Serialize;
-
-#[derive(Debug, Clone, Serialize)]
-#[serde(rename_all = "camelCase")]
-pub struct SystemSkillTreeFile {
-    pub id: String,
-    pub relative_path: String,
-    pub language: String,
-}
